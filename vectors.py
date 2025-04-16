5a99ccbf9cc50c986e2a7f7580ad1ffca0987f07
Nikolai40Kuznetsov <nikolaikuznetsov@tutamail.com>
2025-04-16 14:39:31 +0300

    yy

--- vectors.py
+++ vectors.py
@@ -10,7 +10,7 @@ class Vector2D:
         return (self.x * other.x + self.y * other.y)
     
     def __str__(self):
-        return f"({self.x},{self.y})"
+        return f"({self.x}, {self.y})"
 
 class Vector3D:
     def __init__(self, x, y, z):
@@ -31,9 +31,10 @@ class Vector3D:
         return (q * q / ((self.x * self.x + self.y * self.y + self.z * self.z) * (other.x * other.x + other.y * other.y + other.z * other.z)) ** 0.5)
     
     def __str__(self):
-        return f"({self.x},{self.y},{self.z})"
+        return f"({self.x}, {self.y}, {self.z})"
     
 v1 = Vector3D(1, 2, 3)
 v2 = Vector3D(4, 5, 6)
 print(v1 * v2)
+print(v1 @ v2)
                     
